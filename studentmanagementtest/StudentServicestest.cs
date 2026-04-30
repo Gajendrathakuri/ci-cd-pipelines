df1b0f3b6b798e14c12ac0cc013f955bf73cce64
Thakuri <gaju76960@gmail.com>
2026-04-30 13:34:13 +0545

    Added xunit test project for ci pipline

---
+++ studentmanagementtest/StudentServicestest.cs
@@ -0,0 +1,20 @@
+﻿using studentManagement.Service;
+
+namespace studentmanagementtest
+{
+    public class StudentServicestest
+    {
+        [Fact]
+        public void GetAllStudent()
+        {
+            // Arrange
+            var service = new StudentService();
+            // Act
+            var student = service.GetAllStudents();
+            // Assert
+            Assert.NotNull(student);
+            Assert.NotEmpty(student);
+            Assert.True(student.Count >= 2);
+        }
+    }
+}
